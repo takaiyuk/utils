65680b1bb7b8137f82a502af8cbee92f5b26bf5c
Taka Ishikawa <32270184+takaiyuk@users.noreply.github.com>
2020-07-02 08:37:46 +0900

    Update timer.py

--- py/timer.py
+++ py/timer.py
@@ -1,14 +1,17 @@
+import logging
 import time
 from contextlib import contextmanager
 
 
 @contextmanager
-def timer(name: str) -> None:
+def timer(name: str, logger: logging.Logger = None) -> None:
     """
     Parameters
     ----------
     name : str
         the name of the function that measures time。
+    logger: logging.Logger
+        logger if you want to use. If None, print() will be used.
 
     Examples
     --------
@@ -16,5 +19,9 @@ def timer(name: str) -> None:
             modeling()
     """
     t0 = time.time()
+    message = f"[{name}] done in {time.time() - t0:.1f} s"
     yield
-    print(f"[{name}] done in {time.time() - t0:.0f} s")
+    if logger is not None:
+        logger.info(message)
+    else:
+        print(message)
