f724d472600dfc275c0fd12b69d1fcc1bec948d1
Taka Ishikawa <32270184+takaiyuk@users.noreply.github.com>
2020-08-01 23:03:43 +0900

    Update download.sh

--- shell/download.sh
+++ shell/download.sh
@@ -3,9 +3,9 @@
 COMPETITION_NAME=$1
 if [ -n "$COMPETITION_NAME" ]; then
   echo ${COMPETITION_NAME}
-  kaggle competitions download ${COMPETITION_NAME}} -p .
-  unzip ${COMPETITION_NAME}}.zip -d data/raw
-  rm ${COMPETITION_NAME}}.zip
+  kaggle competitions download ${COMPETITION_NAME} -p .
+  unzip ${COMPETITION_NAME}.zip -d data/raw
+  rm ${COMPETITION_NAME}.zip
 else
   echo "COMPETITION_NAME should be provided"
   exit 1
